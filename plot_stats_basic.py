9c4476f0896b8f612535d988b0861d71e0a3f526
naggeluide <67690069+naggeluide@users.noreply.github.com>
2020-07-25 10:24:31 +0200

    added wordcloud()

--- plot_stats_basic.py
+++ plot_stats_basic.py
@@ -2,7 +2,8 @@ from matplotlib.dates import date2num,num2date,DateFormatter
 import pandas as pd
 import os
 import numpy as np
-
+from wordcloud import WordCloud, STOPWORDS
+from collections import Counter
 
 CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
                   '#f781bf', '#a65628', '#984ea3',
@@ -45,3 +46,54 @@ def all_stats_plot_work(xls_file,sheet_title,subs=False,wc=False,kfac=10.,perhit
   plt.gcf().autofmt_xdate()
   fig.show()
 
+def wordcloud(xls,field='Additional Tags',fandom='all'):
+    ''' make a wordcloud of your tags, characters, etc (metadata) for a particular fandom or for all works'''
+    dfdict={}
+    sheet_names=pd.ExcelFile(xls).sheet_names
+    sheet_names.remove('Totals')
+    for s in sheet_names:
+        dfdict[s]=pd.read_excel(xls,s)
+
+    #does field exist?
+    if field not in dfdict[s].keys():
+        print 'Field ' + field + ' does not exist! please pick one of: ' + dfdict[s].keys() #should throw a proper exception instead...
+        return
+
+    #select data
+    words=[]
+    if fandom !='all':
+        flist=[dfdict[s]['Fandom'][0] for s in sheet_names]
+        slist=[]
+        for f,s in zip(flist,sheet_names):
+            if fandom in f:
+                slist.append(s)
+    else:
+        slist=sheet_names
+
+    for s in slist:
+        all_vals=dfdict[s][field].values
+        words.append([v for v in all_vals if type(v) !=float])
+
+    #if words is [] then fandom probably written wrong
+    llist=[w for w in words[0]]#buff=",".join(words[0])
+    for ww in words[1:]:
+        for w in ww:
+            llist.append(w)
+
+    freqs=Counter(llist)
+
+    #comment_words = ''
+    stopwords = set(STOPWORDS)
+
+    wordcloud = WordCloud(width = 800, height = 800,
+                regexp=r"\w[\w' ],",
+                background_color ='white',
+                stopwords = stopwords,
+                min_font_size = 10).generate_from_frequencies(freqs)
+
+    # plot the WordCloud image
+    fig,ax=plt.subplots(figsize = (8, 8), facecolor = None)
+    ax.imshow(wordcloud)
+    ax.axis("off")
+    fig.tight_layout(pad = 0)
+    fig.show()
